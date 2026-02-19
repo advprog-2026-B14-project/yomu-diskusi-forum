80cfb8b1909c84b2f737761a4845f10b2ef95564
vd <mrafighalibf@gmail.com>
2026-02-20 00:36:05 +0700

    chore: initialize project

---
+++ src/main/java/id/ac/ui/cs/advprog/yomuforum/YomuForumApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomuforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/yomuforum/YomuForumApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomuforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomuForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
