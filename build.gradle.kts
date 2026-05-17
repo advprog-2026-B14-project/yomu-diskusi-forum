5bbfb1da0ca6772e2b73b01d4602ec20c43ffe72
moondiverc <nezzaluna10@gmail.com>
2026-05-17 17:11:01 +0700

    feat: add gradle.lockfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ repositories {
     mavenCentral()
 }
 
+dependencyLocking {
+    lockAllConfigurations()
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
