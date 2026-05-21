52fbe8fec0d8afed442b6e0556b7d02a05bd8235
moondiverc <nezzaluna10@gmail.com>
2026-05-21 17:13:10 +0700

    fix: override DB config to use H2 in functional tests

--- src/test/java/id/ac/ui/cs/advprog/yomuforum/functional/CommentApiFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/yomuforum/functional/CommentApiFunctionalTest.java
@@ -21,7 +21,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+                "spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+        }
+)
 class CommentApiFunctionalTest {
 
     @Autowired
