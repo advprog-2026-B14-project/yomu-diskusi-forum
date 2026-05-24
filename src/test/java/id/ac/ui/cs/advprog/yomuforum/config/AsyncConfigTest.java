d667eadd1ab456624f5c81ff61c147ef7dc47537
moondiverc <nezzaluna10@gmail.com>
2026-05-24 19:29:36 +0700

    feat: deploy

--- src/test/java/id/ac/ui/cs/advprog/yomuforum/config/AsyncConfigTest.java
+++ src/test/java/id/ac/ui/cs/advprog/yomuforum/config/AsyncConfigTest.java
@@ -3,7 +3,9 @@ package id.ac.ui.cs.advprog.yomuforum.config;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Executor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -15,13 +17,13 @@ class AsyncConfigTest {
         Executor executor = config.taskExecutor();
         
         assertNotNull(executor);
-        assertTrue(executor instanceof org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor);
+        assertTrue(executor instanceof ThreadPoolTaskExecutor);
         
-        org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor threadPool = (org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor) executor;
+        ThreadPoolTaskExecutor threadPool = (ThreadPoolTaskExecutor) executor;
         
         // Assert some configurations
-        org.junit.jupiter.api.Assertions.assertEquals(2, threadPool.getCorePoolSize());
-        org.junit.jupiter.api.Assertions.assertEquals(4, threadPool.getMaxPoolSize());
-        org.junit.jupiter.api.Assertions.assertEquals("yomu-async-", threadPool.getThreadNamePrefix());
+        assertEquals(2, threadPool.getCorePoolSize());
+        assertEquals(4, threadPool.getMaxPoolSize());
+        assertEquals("yomu-async-", threadPool.getThreadNamePrefix());
     }
 }
